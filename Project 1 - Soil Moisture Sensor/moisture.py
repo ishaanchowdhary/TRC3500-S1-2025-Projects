26031660addeef3b72a5ac43a17f3bb9122996e2
ishaanchowdhary <ishaanguhachowdhary@gmail.com>
2025-03-21 14:46:54 +1100

    Update moisture.py
    Changed to COM5

--- Project 1 - Soil Moisture Sensor/moisture.py
+++ Project 1 - Soil Moisture Sensor/moisture.py
@@ -47,7 +47,7 @@ print("-------------------------------------------------------------------------
 
 # Calculating and outputtign Moisture Percentage
 
-ser = serial.Serial("COM4", 115200, timeout=1)
+ser = serial.Serial("COM5", 115200, timeout=1)
 
 while True:
     data = ser.readline().decode("utf-8").strip()
