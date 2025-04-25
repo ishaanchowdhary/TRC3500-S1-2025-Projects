7870efc066373bdba54727a3e1af097d36fd8ea9
Emanuel Risso <emanueljrisso@tutanota.com>
2025-04-25 20:49:21 +1000

    Update project_2.py

--- Project 2 - Pin-drip Sensor/project_2.py
+++ Project 2 - Pin-drip Sensor/project_2.py
@@ -1,34 +1,60 @@
-'''
-Written By: Ishaan Chowdhary
-Last modified: 25/04/2025
-Last Modifier: Ishaan Chowdhary
-'''
+# File Name: pin_drop.py
+# Authors: Ishaan Chowdhary, Emanuel Risso
+# Created: 25/04/2025
+# Last Modified: 25/04/2025
+# Python v. 3.13.2
 
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+''' File Overview '''
+
+# This program reads live ADC values from the STM through a serial connection
+# and converts them to voltage. If a HIGH signal is received, a vibration
+# event is started (lasting 3 seconds). During this event, the real time and
+# magnitude of all vibrations (bounces) will be printed to the terminal.
+
+''' Module Imports '''
+
 import serial
+from datetime import datetime
 
-ser = serial.Serial("COM5", 115200, timeout=1)
+''' Receive, format, and print the data from the STM ADC '''
 
-debounce_duration = 5000000  # micro seconds
-last_peak_time = 0     # initialize last peak detection time
+# Set the serial connection to use the correct port and baud rate
+ser = serial.Serial('COM5', 115200, timeout=1)
+# Set the debounce delay (ms) to specify one event for multiple bounces
+debounce_duration = 3000
+# Set the initial last peak detection time to 0 (ms)
+last_peak_time = 0.0
+# Set the voltage threshold (V) to qualify as a HIGH signal from the STM
+threshold_voltage = 3.24
 
-threshold_voltage = 3.2  # Adjust this threshold based on your needs
+# Initiate an infinite loop
 while True:
-    data = ser.readline().decode("utf-8").strip().split(",")
+    # Set the current time
+    current_time = datetime.now()
+    # Set the current time (ms) for logic use
+    now = current_time.timestamp() * 1000
+    # Format the time to print
+    print_time = (current_time.strftime('%H:%M:%S:%f')[:-3])
+    # Read each line from the ADC serial connection output
+    data = ser.readline().decode('utf-8').strip()
+    # Convert the data to float type
+    adc_value = float(data)
+    # Convert the ADC data to a specified voltage range
+    voltage = ((adc_value / 4095) * 3.3)
 
-    adc_value = float(data[0])
-    time_value = float(data[1])
-    voltage = adc_value * 3.3 /4096
-    # Debouncing signal
-    if last_peak_time + debounce_duration > time_value:
-        print(f"Time: {time_value:.2f} Voltage: {voltage:.2f}")
+    # If the current time minus the last peak time is less than 3 seconds
+    if ((now - last_peak_time) < debounce_duration):
+        # Exit the if elif statement
         pass
-    # Detect Signal
-    if voltage >= threshold_voltage:
-        print("-------------------------------------")
-        print("EVENT DETECTED")
-        print("-------------------------------------")
-        last_peak_time = time_value
-    print(f"Time: {time_value:.2f} Voltage: {voltage:.2f}")
+    # If the voltage reading is above the threshold
+    elif voltage >= threshold_voltage:
+        # Print that a vibration event has been detected
+        print('-------------------------------------------------')
+        print('VIBRATION EVENT DETECTED')
+        print('-------------------------------------------------')
+        # Set the most recent peak time  to the current time
+        last_peak_time = now
+    # If the voltage is HIGH in this event (lasting 3 seconds max)
+    if voltage != 0:
+        # Print the current time and voltage
+        print(f'Time: {print_time} H:MM:SS:ms --- Voltage: {voltage:.2f} V')
