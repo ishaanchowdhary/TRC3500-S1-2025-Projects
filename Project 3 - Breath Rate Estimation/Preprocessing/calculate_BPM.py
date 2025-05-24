69375a05e3e5850b6e595c158942de73c1a5c0e4
ishaanchowdhary <ishaanguhachowdhary@gmail.com>
2025-05-24 21:56:47 +1000

    Update Preprocessing Files Documentation

--- Project 3 - Breath Rate Estimation/Preprocessing/calculate_BPM.py
+++ Project 3 - Breath Rate Estimation/Preprocessing/calculate_BPM.py
@@ -2,7 +2,7 @@
 Written by: Ishaan Guha Chowdhary (33115303)
 Last edited: 24/05/2025
 
-Functions to calculate Breath Rates from Sphygmomanometer, Strain Sensor, Thermistor and Super-Imposed State
+Functions to calculate Breath Rates from Sphygmomanometer, Strain Sensor, Thermistor and Super-Imposed State.
 '''
 
 def calc_sphyg_bpm():
--- Project 3 - Breath Rate Estimation/Preprocessing/super_imposed_state.py
+++ Project 3 - Breath Rate Estimation/Preprocessing/super_imposed_state.py
@@ -2,7 +2,7 @@
 Written by: Ishaan Guha Chowdhary (33115303)
 Last edited: 24/05/2025
 
-Create Super-Imposed State based on Sphygmomanometer ground truth from Strain Sensor and Thermistor.
+Calculates Super-Imposed State based on Sphygmomanometer ground truth from Strain Sensor and Thermistor.
 '''
 
 print("Pinda hoo")
