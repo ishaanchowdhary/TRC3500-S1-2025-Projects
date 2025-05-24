9a59fba6e68d37d86aa15410cec6ad9858c81286
ishaanchowdhary <ishaanguhachowdhary@gmail.com>
2025-05-24 21:53:45 +1000

    Create calculate_BPM.py
    For calculating Breath Rates from Individual Sensors and Super-Imposed State

---
+++ Project 3 - Breath Rate Estimation/Preprocessing/calculate_BPM.py
@@ -0,0 +1,18 @@
+'''
+Written by: Ishaan Guha Chowdhary (33115303)
+Last edited: 24/05/2025
+
+Functions to calculate Breath Rates from Sphygmomanometer, Strain Sensor, Thermistor and Super-Imposed State
+'''
+
+def calc_sphyg_bpm():
+    print("Pinda Hoo")
+
+def calc_strain_bpm():
+    print("Pinda Hoo")
+
+def calc_therm_bpm():
+    print("Pinda Hoo")
+
+def calc_state_bpm():
+    print("Pinda Hoo")
